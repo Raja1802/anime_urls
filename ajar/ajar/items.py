eef11974712a34334c59dd7c4b71cc8d3c87de51
Raja1802 <rajagandla16@gmail.com>
2019-03-01 17:32:12 +0000

    zcszd

--- ajar/ajar/items.py
+++ ajar/ajar/items.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Define here the models for your scraped items
@@ -7,65 +8,70 @@
 
 import scrapy
 
+
 # 9naime anime info downloader
 
-
 class AjarItem(scrapy.Item):
-        name = scrapy.Field()
-        image = scrapy.Field()
-        synonyms = scrapy.Field()
-        types = scrapy.Field()
-        studios = scrapy.Field()
-        aired = scrapy.Field()
-        status = scrapy.Field()
-        scores = scrapy.Field()
-        premired = scrapy.Field()
-        durination = scrapy.Field()
-        quality = scrapy.Field()
-        gener = scrapy.Field()
-        about = scrapy.Field()
-        tags = scrapy.Field()
-        cover = scrapy.Field()
+
+    name = scrapy.Field()
+    image = scrapy.Field()
+    synonyms = scrapy.Field()
+    types = scrapy.Field()
+    studios = scrapy.Field()
+    aired = scrapy.Field()
+    status = scrapy.Field()
+    scores = scrapy.Field()
+    premired = scrapy.Field()
+    durination = scrapy.Field()
+    quality = scrapy.Field()
+    gener = scrapy.Field()
+    about = scrapy.Field()
+    tags = scrapy.Field()
+    cover = scrapy.Field()
+
 
 # pixabay images links grabber
 
-
 class AjarPixabayItem(scrapy.Item):
-        img_srcset = scrapy.Field()
-        img_src = scrapy.Field()
-        img_tags = scrapy.Field()
+
+    img_srcset = scrapy.Field()
+    img_src = scrapy.Field()
+    img_tags = scrapy.Field()
+
+
 # gogo anime urls collector
 
-
 class AjarGogoanimeItem(scrapy.Item):
-        anime_name = scrapy.Field()
-        name_episode = scrapy.Field()
-        download_url = scrapy.Field()
-        server_name_1 = scrapy.Field()
-        server_url_1 = scrapy.Field()
-        server_name_2 = scrapy.Field()
-        server_url_2 = scrapy.Field()
-        server_name_3 = scrapy.Field()
-        server_url_3 = scrapy.Field()
-        server_name_4 = scrapy.Field()
-        server_url_4 = scrapy.Field()
-        server_name_5 = scrapy.Field()
-        server_url_5 = scrapy.Field()
-        server_name_6 = scrapy.Field()
-        server_url_6 = scrapy.Field()
-        server_name_7 = scrapy.Field()
-        server_url_7 = scrapy.Field()
-        server_name_8 = scrapy.Field()
-        server_url_8 = scrapy.Field()
-        server_url_9 = scrapy.Field()
+
+    anime_name = scrapy.Field()
+    name_episode = scrapy.Field()
+    download_url = scrapy.Field()
+    server_name_1 = scrapy.Field()
+    server_url_1 = scrapy.Field()
+    server_name_2 = scrapy.Field()
+    server_url_2 = scrapy.Field()
+    server_name_3 = scrapy.Field()
+    server_url_3 = scrapy.Field()
+    server_name_4 = scrapy.Field()
+    server_url_4 = scrapy.Field()
+    server_name_5 = scrapy.Field()
+    server_url_5 = scrapy.Field()
+    server_name_6 = scrapy.Field()
+    server_url_6 = scrapy.Field()
+    server_name_7 = scrapy.Field()
+    server_url_7 = scrapy.Field()
+    server_name_8 = scrapy.Field()
+    server_url_8 = scrapy.Field()
+    server_url_9 = scrapy.Field()
 
 
 class AjarAnilistItem(scrapy.Item):
-	    image_url = scrapy.Field()
+
+    image_url = scrapy.Field()
 
 
 class AjarWallAlphaItem(scrapy.Item):
-			image_url = scrapy.Field()
-			image_tags = scrapy.Field()
-			image_type = scrapy.Field()
 
+    image_url = scrapy.Field()
+    image_tags = scrapy.Field()
+    image_type = scrapy.Field()
